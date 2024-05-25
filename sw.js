65e15935a43d17308ba79f0a6457dad034edaea0
Ahmad Muzakki <yitna.yuana@gmail.com>
2024-05-25 18:09:00 +0700

    first commit

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@ self.addEventListener('install', function(event) {
 	event.waitUntil(
 	caches.open('app-cache').then(function(cache) {
 		return cache.addAll([
-			'/',
+			// '/',
 			'index.html',
 			'style.css',
 			'script.js',
