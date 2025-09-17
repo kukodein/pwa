39aa88303c713305586b91f78284f7e9e4ccefaf
Ahmad Muzakki <yitna.yuana@gmail.com>
2025-09-17 09:00:36 +0700

    Update basic PWA

--- sw.js
+++ sw.js
@@ -1,27 +1,26 @@
-var cacheName = 'kukode-pwa';
+var cacheName = 'hello-pwa';
 var filesToCache = [
-	'/',
-	'/index.html',
-	'/css/style.css',
-	'/js/main.js',
-	'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css'
+  '/',
+  '/index.html',
+  '/css/style.css',
+  '/js/main.js'
 ];
 
 /* Start the service worker and cache all of the app's content */
 self.addEventListener('install', function(e) {
-	e.waitUntil(
-		caches.open(cacheName).then(function(cache) {
-			return cache.addAll(filesToCache);
-		})
-	);
-	self.skipWaiting();
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+  self.skipWaiting();
 });
 
 /* Serve cached content when offline */
 self.addEventListener('fetch', function(e) {
-	e.respondWith(
-		caches.match(e.request).then(function(response) {
-			return response || fetch(e.request);
-		})
-	);
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
 });
